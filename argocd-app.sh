fda02524be6554716b5baf46f23335bb04bc5ace
Soumya Sekhar Dey <soumyasekhardey@gmail.com>
2025-04-29 12:49:46 -0400

    corrected values.yaml file path in argocd create script

--- argocd-app.sh
+++ argocd-app.sh
@@ -4,7 +4,7 @@ echo "Creating ArgoCD Application from GitHub"
 argocd app create fastapi-helm-app \
   --repo https://github.com/soumya-sekhar-dey/kubernetes-helm-python-app.git \
   --path helm/fastapi-chart \
-  --values helm/fastapi-chart/values.yaml \
+  --values values.yaml \
   --dest-server https://kubernetes.default.svc \
   --dest-namespace default \
   --sync-policy automated
